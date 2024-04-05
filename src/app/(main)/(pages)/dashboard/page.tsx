071c347ef040258dc4daf79ef675496fe0c8659a
chenma <chen.ma.dev@gmail.com>
2024-04-05 20:20:47 +1100

    modify the layout of dashboard so it would not effect on the links in homepage

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Layout from '@/components/mlayout'
 
 const DashboardPage = () => {
   return (
-    <div className="flex flex-col gap-4 relative">
-      
+    <Layout><div className="flex flex-col gap-4 relative">
+
       <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
         Dashboard
       </h1>
     </div>
+    </Layout>
   )
 }
-
 export default DashboardPage
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,4 +1,5 @@
 import ProfileForm from '@/components/forms/profile-form'
+import Layout from '@/components/mlayout'
 import React from 'react'
 //import ProfilePicture from './_components/profile-picture'
 //import { db } from '@/lib/db'
@@ -7,54 +8,54 @@ import React from 'react'
 type Props = {}
 
 const Settings = async (props: Props) => {
-//   const authUser = await currentUser()
-//   if (!authUser) return null
+  //   const authUser = await currentUser()
+  //   if (!authUser) return null
 
-//   const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
-//   const removeProfileImage = async () => {
-//     'use server'
-//     const response = await db.user.update({
-//       where: {
-//         clerkId: authUser.id,
-//       },
-//       data: {
-//         profileImage: '',
-//       },
-//     })
-//     return response
-//   }
+  //   const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
+  //   const removeProfileImage = async () => {
+  //     'use server'
+  //     const response = await db.user.update({
+  //       where: {
+  //         clerkId: authUser.id,
+  //       },
+  //       data: {
+  //         profileImage: '',
+  //       },
+  //     })
+  //     return response
+  //   }
 
-//   const uploadProfileImage = async (image: string) => {
-//     'use server'
-//     const id = authUser.id
-//     const response = await db.user.update({
-//       where: {
-//         clerkId: id,
-//       },
-//       data: {
-//         profileImage: image,
-//       },
-//     })
+  //   const uploadProfileImage = async (image: string) => {
+  //     'use server'
+  //     const id = authUser.id
+  //     const response = await db.user.update({
+  //       where: {
+  //         clerkId: id,
+  //       },
+  //       data: {
+  //         profileImage: image,
+  //       },
+  //     })
 
-//     return response
-//   }
+  //     return response
+  //   }
 
-//   const updateUserInfo = async (name: string) => {
-//     'use server'
+  //   const updateUserInfo = async (name: string) => {
+  //     'use server'
 
-//     const updateUser = await db.user.update({
-//       where: {
-//         clerkId: authUser.id,
-//       },
-//       data: {
-//         name,
-//       },
-//     })
-//     return updateUser
-//   }
+  //     const updateUser = await db.user.update({
+  //       where: {
+  //         clerkId: authUser.id,
+  //       },
+  //       data: {
+  //         name,
+  //       },
+  //     })
+  //     return updateUser
+  //   }
 
   return (
-    <div className="flex flex-col gap-4">
+    <Layout><div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
         <span>Settings</span>
       </h1>
@@ -66,17 +67,17 @@ const Settings = async (props: Props) => {
           </p>
         </div>
         {/* <ProfilePicture
-          onDelete={removeProfileImage}
-          userImage={user?.profileImage || ''}
-          onUpload={uploadProfileImage}
-        /> */}
+        onDelete={removeProfileImage}
+        userImage={user?.profileImage || ''}
+        onUpload={uploadProfileImage}
+      /> */}
         <ProfileForm
           user={'user'}
           onUpdate={'updateUserInfo'}
         />
       </div>
     </div>
+    </Layout>
   )
 }
-
 export default Settings
